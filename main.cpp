7b6f87aeedcf86a7c0d2975c9e61035ca49cdccc
ahnufmir <mirahnaf55@gmail.com>
2025-09-18 23:51:25 +0500

    Practise old stuff

---
+++ main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+// class Shape{
+//     public:
+//     virtual double area() const = 0;
+// };
+
+// class Rectangle : public Shape{
+//     int a,b;
+//     public:
+//     Rectangle(int a , int b) : a(a), b(b) {}
+//     double area() const override{
+//         return (a*b);
+//     }
+// };
+
+// class Circle : public Shape{
+//     int r;
+//     public:
+//     Circle(int r) : r(r) {}
+//     double area() const override{
+//         return(3.14*r*r);
+// }
+// };
+
+int main(){
+    // int a = 10;
+    // int b = 5;
+    // Shape *s = new Rectangle(a,b);
+    // Shape *sh = new Circle(a);
+
+    // cout<< s->area() << endl;
+    // cout << sh->area() << endl;
+    
+    // delete s;
+    // delete sh;
+
+        // int oldArr[] = {1,2,3,4,5,6};
+        // int *newArr = new int[10];
+        // int value;
+
+        // for(int i = 0; i <6; i++){
+        //     value = oldArr[i];
+        //     newArr[i] = value;        
+        // }
+        // for (int i = 0; i < 5; i++)
+        // {
+        //     cout << newArr[i] << endl;
+        // }
+        // cout << "end" << endl;
+        
+        // int input;
+        // for (int i = 6; i < 10; i++)
+        // {
+        //     cout << "Enter the number you want to add in newArr" << endl;
+        //     cin >> input;
+        //     newArr[i] = input;
+        // }
+        // for (int i = 0; i < 10; i++)
+        // {
+        //     cout << newArr[i] << endl;
+        // }
+    
+        // int b = 10;
+        // int *a = &b;
+        // int **c = &a;
+        // cout << a << endl;
+        // cout << sizeof(c) << endl;
+        // cout << sizeof(**c) << endl;
+
+
+// All above code, i was just practising, from below i will start from very fundamentals
+
+        for(int i = 5; i >1; i--){
+            cout << i << endl;
+        }
+
+
+        
+}
