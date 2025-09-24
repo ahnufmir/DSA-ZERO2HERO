63a9f9f0a03672c9484e1e49ff829b9100d7eea5
ahnufmir <mirahnaf55@gmail.com>
2025-09-24 11:00:44 +0500

    Practising 2d dynamic array

--- MyArray.h
+++ MyArray.h
@@ -4,7 +4,7 @@
 #include <iostream>
 using namespace std;
 
-template <typename T> // Template Declaration
+template <typename T>
 class MyArray
 {
 private:
@@ -15,21 +15,28 @@ private:
 public:
     MyArray()
     {
-        capacity = 5;
         size = 0;
-        data = new T[capacity]; // Dynamically Allocated Array
+        capacity = 5;
+        data = new T[capacity];
+    }
+    MyArray(int cap)
+    {
+        size = 0;
+        capacity = cap;
+        data = new T[capacity];
     }
     ~MyArray()
     {
         delete[] data;
     }
+
     void append(T value)
     {
         if (size == capacity)
         {
             capacity *= 2;
             T *temp = new T[capacity];
-            for (int i = 0; i < capacity; i++)
+            for (int i = 0; i < size; i++)
             {
                 temp[i] = data[i];
             }
@@ -51,22 +58,16 @@ public:
             data = temp;
             capacity = capacity * 2;
         }
-        cout << "    SIZE = " << size;
-        for (int i = size; i>0; i--)
+        cout << "    SIZE = " << size << endl;
+        for (int i = size; i > 0; i--)
         {
-            data[i] = data[i-1];
+            data[i] = data[i - 1];
         }
         data[0] = value;
-        size +=1 ;
-        cout  << "    NEW SIZE = " << size;
+        size += 1;
+        cout << "    NEW SIZE = " << size << endl;
     }
-    void display()
-    {
-        for (int i = 0; i < size; i++)
-        {
-            cout << data[i] << endl;
-        }
-    }
+
     int get_size()
     {
         return size;
@@ -79,7 +80,204 @@ public:
     {
         return data;
     }
-    
+    void insertAt(int index, T value)
+    {
+        if (index >= 0 && index <= size)
+        {
+            if (size >= capacity)
+            {
+                T *temp = new T[capacity * 2];
+                for (int i = 0; i < size; i++)
+                {
+                    temp[i] = data[i];
+                }
+                delete[] data;
+                data = temp;
+                capacity = capacity * 2;
+            }
+            cout << "    SIZE = " << size << endl;
+            for (int i = size; i > index; i--)
+            {
+                data[i] = data[i - 1];
+            }
+            data[index] = value;
+            size += 1;
+            cout << "    NEW SIZE = " << size << endl;
+        }
+        else
+            cout << "Error" << endl;
+    }
+
+    void removeAt(int index)
+    {
+        if (index >= 0 && index < size)
+        {
+            if (size >= capacity)
+            {
+                T *temp = new T[capacity * 2];
+                for (int i = 0; i < size; i++)
+                {
+                    temp[i] = data[i];
+                }
+                delete[] data;
+                data = temp;
+                capacity = capacity * 2;
+            }
+            cout << "    SIZE = " << size << endl;
+            for (int i = index; i < size - 1; i++)
+            {
+                data[i] = data[i + 1];
+            }
+            size -= 1;
+            cout << "    NEW SIZE = " << size << endl;
+        }
+        else
+            cout << "Error" << endl;
+    }
+    void display()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            cout << data[i] << endl;
+        }
+    }
+    void sortArray(bool asc)
+    {
+        if (asc)
+        {
+            for (int h = 0; h < size - 1; h++)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    if (data[i] > data[i + 1])
+                    {
+                        int temp = data[i + 1];
+                        data[i + 1] = data[i];
+                        data[i] = temp;
+                    }
+                }
+            }
+        }
+        else
+        {
+            for (int h = 0; h < size - 1; h++)
+            {
+                for (int i = 0; i < size - h - 1; i++)
+                {
+                    if (data[i] < data[i + 1])
+                    {
+                        int temp = data[i + 1];
+                        data[i + 1] = data[i];
+                        data[i] = temp;
+                    }
+                }
+            }
+        }
+    }
+    void removeDuplicates()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = i + 1; j < size; j++)
+            {
+                if (data[i] == data[j])
+                {
+                    cout << "    SIZE = " << size << endl;
+                    for (int k = j; k < size - 1; k++)
+                    data[k] = data[k + 1];
+                size -= 1;
+                i -= 1;
+                cout << "    NEW SIZE = " << size << endl;
+                }
+            }
+        }
+    }
+    void reverse()
+    {
+        for (int i = 0; i < size / 2; i++)
+        {
+            T temp = data[i];
+            data[i] = data[size - i - 1];
+            data[size - i - 1] = temp;
+        }
+    }
+    friend ostream &operator<<(ostream &out, const MyArray<T> &obj)
+    {
+        for (int i = 0; i < obj.size; i++)
+        {
+            out << obj.data[i] << endl;
+        }
+        return out;
+    }
+    T &operator[](int index)
+    {
+        if (index < 0 || index > size)
+        {
+            cout << "Invalid index or out of boundary access" << endl;
+            static T dummy{};
+            return dummy;
+        }
+            return data[index];
+    }
+    bool operator==(const MyArray &obj) const
+    {
+        bool check = true;
+        if (this->size != obj.size)
+        {
+            check = false;
+        }
+        else if (this->size == obj.size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (this->data[i] != obj.data[i])
+                {
+                    check = false;
+                }
+            }
+        }
+        return check;
+    }
+    MyArray<T> operator+(const MyArray<T> &obj)
+    {
+        int newSize = this->size + obj.size;
+        cout << "New Size " << newSize << endl;
+        MyArray<T> newArr;
+        cout << "This size : "  << this->size << endl;
+        cout << "Obj size : "  << obj.size << endl;
+        for (int i = 0; i < this->size; i++)
+        {
+            newArr.append(this->data[i]);
+        }
+        for (int j = 0; j < obj.size; j++)
+        {
+            newArr.append(obj.data[j]);
+        }
+        // cout << "This size : "  << this->size << endl;
+        // cout << "Obj size : "  << obj.size << endl;
+        // cout << "New Size " << newSize << endl;
+        // for (int i = 0; i < newArr.size; i++)
+        // {
+        //     cout<<newArr[i] << endl;
+        // }
+        return newArr;
+        
+    }
+    void menu()
+    {
+        cout << "============ MENU ====================" << endl;
+        cout << "0. Display " << endl;
+        cout << "1. Append at Index " << endl;
+        cout << "2. Remove at Index " << endl;
+        cout << "3. Sorting " << endl;
+        cout << "4. Remove Duplicates " << endl;
+        cout << "5. Reverse " << endl;
+        cout << "6. << Operator Overloading " << endl;
+        cout << "7. [] Operator Overloading " << endl;
+        cout << "8. == Operator Overloading " << endl;
+        cout << "9. + Operator Overloading " << endl;
+        cout << "--------------------------------------" << endl;
+    }
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,50 +1,93 @@
 #include "MyArray.h"
 
-int main(){
+int main()
+{
     MyArray<int> obj;
-    char choice = 'y';
 
-    while (choice == 'y')
+    char ch = 'y';
+
+    while (ch == 'y')
     {
-        int select;
-        cout << "===================== MENU =====================" << endl;
-        cout << "Append Element (press 1)" << endl;
-        cout << "Prepend Element (press 2)" << endl;
-        cout << "Display All Elements (press 3)" << endl;
-        cout << "Get Current Size Element (press 4)" << endl;
-        cout << "Get Current Capacity Element (press 5)" << endl;
-        cin >> select;
-
-        if(select == 1){
-            int value;
-            cout << "Enter the value " << endl;
-            cin >> value;
-            obj.append(value);   // Append function
+        obj.menu();
+        int n;
+        cin >> n;
+        if (n == 1)
+        {
+            int index, value;
+            cout << "Enter index and value" << endl;
+            cin >> index >> value;
+            obj.insertAt(index, value);
+        }
+        if (n == 2)
+        {
+            int index;
+            cout << "Enter index and value" << endl;
+            cin >> index;
+            obj.removeAt(index);
+        }
+        if (n == 0)
+        {
+            obj.display();
+        }
+        if (n == 3)
+        {
+            bool b;
+            int n;
+            cout << "Press 0 for descending order sorting and 1 for ascending order sorting " << endl;
+            cin >> n;
+            if (n == 1)
+            {
+                b = true;
+                obj.sortArray(b);
+            }
+            else if (n == 0)
+            {
+                b = false;
+                obj.sortArray(b);
+            }
+        }
+        if (n == 5)
+        {
+            obj.removeDuplicates();
+        }   
+        if (n == 6)
+        {
+            cout << obj << endl;
+        }
+        if (n == 7)
+        {
+            obj[0] = 50;
+            cout << obj[0] << endl;
+        }
+        if (n == 8)
+        {
+            MyArray<int> arr1;
+            MyArray<int> arr2;
+            arr1.append(1);
+            arr1.append(2);
+            arr2.append(1);
+            arr2.append(2);
+            if (arr1 == arr2)
+            cout << "Both arrays are equal" << endl;
+            else
+            cout << "Both arrays are not equal" << endl;
+        }
+        if (n == 9)
+        {
+            MyArray<int> arr1;
+            MyArray<int> arr2;
+            arr1.append(1);
+            arr1.append(2);
+            arr1.append(3);
+            arr2.append(4);
+            arr2.append(5);
+            MyArray<int> arr3 = arr1 + arr2;
+            cout << arr3 << endl;
         }
         
-        else if(select == 2){
-            int value;
-            cout << "Enter the value " << endl;
-            cin >> value;
-            obj.prepend(value);   // Prepend Function
-        }
-
-        else if(select == 3){
-            obj.display();    // Display Function
-        }
-
-        else if(select == 4){
-            cout << obj.get_size() << endl;       // Get Size Function
-        }
-
-        else if(select == 5){
-            cout << obj.get_capacity()  << endl;       // Get Capacity Function
-        }
- 
-        cout << "Do you want to do this stuff again? "<< endl;
-        cin >> choice;
-        
+        cout << "Do you want to continue " << endl;
+        cin >> ch;
     }
-    
 
+    return 0;
 }
