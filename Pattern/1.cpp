4b6f9343fe8d2467b86e64352ecbaf5ee3dcfcf0
ahnufmir <mirahnaf55@gmail.com>
2025-09-25 23:21:00 +0500

    Patterns Practise

--- Pattern/1.cpp
+++ Pattern/1.cpp
@@ -3,6 +3,27 @@ using namespace std;
 
 int main(){
 
+    // int rows,col;
+    // cout << "Enter the number of rows " << endl;
+    // cin >> rows;
+    // cout << "Enter the number of columns " << endl;
+    // cin >> col;
+    // int arr[rows][col];
+    // for (int i = 0; i < rows; i++)
+    // {
+    //     for (int j = 0; j < col; j++)
+    //     {
+    //         if((j==(col/2)))
+    //             cout <<"1 ";
+    //         else if(i==(rows/2))
+    //             cout << "1 " ;
+    //         else
+    //             cout << "0 ";
+    //     }
+    //     cout << endl;
+        
+    // }
+
     int rows,col;
     cout << "Enter the number of rows " << endl;
     cin >> rows;
@@ -13,15 +34,17 @@ int main(){
     {
         for (int j = 0; j < col; j++)
         {
-            if((j==(col/2)))
+            if((i==0) || i == (rows-1))
                 cout <<"1 ";
-            else if(i==(rows/2))
-                cout << "1 " ;
+            else if(j==0 || j == (col-1))
+                cout << "1 " ;    
             else
                 cout << "0 ";
         }
         cout << endl;
         
     }
+
+
     
 }
