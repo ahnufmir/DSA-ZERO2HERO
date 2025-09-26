e39028483b3cdd01b166029de7152c49740799c5
ahnufmir <mirahnaf55@gmail.com>
2025-09-26 23:42:01 +0500

    Applying Sorting

--- Pattern/1.cpp
+++ Pattern/1.cpp
@@ -45,26 +45,33 @@ int main(){
         
     // }
 
-        int rows,col;
-    cout << "Enter the number of rows " << endl;
-    cin >> rows;
-    cout << "Enter the number of columns " << endl;
-    cin >> col;
-    int arr[rows][col];
-    for (int i = 0; i < rows; i++)
+        int elements;
+    cout << "Enter the number of elements " << endl;
+    cin >> elements;
+    int arr[elements];
+    for (int i = 0; i < elements; i++)
     {
-        for (int j = 0; j < col; j++)
-        {
-            if((i==0) || i == (rows-1))
-                cout <<"0 ";
-            else if(j==0 || j == (col-1))
-                cout << "0 " ;    
-            else
-                cout << "1 ";
-        }
-        cout << endl;
-        
+        cout << "Enter element " << i+1 << endl;
+        cin >> arr[i];
     }
+    for (int i = 0; i < elements-1; i++)
+    {
+        for (int j = i+1; j < elements; j++)
+        {
+            if (arr[j]<arr[i])
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    cout << "----Array :  " << endl;
+    for (int i = 0; i < elements; i++)
+    {
+        cout << arr[i] << endl;
+    }
+    
 
     
 
